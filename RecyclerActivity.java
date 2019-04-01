b67e5af8a7f774a1a6faa941c038384927ec5f2d
ChasShipman <ceshipman@crimson.ua.edu>
2019-04-01 15:33:43 -0500

    Add files via upload
    base code for navigation items. plan of action: replace CreateActivity with Tico's portion. Design Main/Home Activity. Build RecyclerView from Database. Design UserPage

--- RecyclerActivity.java
+++ RecyclerActivity.java
@@ -3,38 +3,38 @@ package com.example.cmfc_nav;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
-    //private TextView mTextMessage;
+public class RecyclerActivity extends AppCompatActivity {
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     //mTextMessage.setText(R.string.title_home);
                     //dont need to open a new activity here
+                    Intent intentMain = new Intent(RecyclerActivity.this, MainActivity.class);
+                    startActivity(intentMain);
                     return true;
                 case R.id.navigation_create:
                     //mTextMessage.setText(R.string.title_create);
-                    Intent intentCreate = new Intent(MainActivity.this, CreateActivity.class);
-                    startActivity(intentCreate);
+                     Intent intentCreate = new Intent(RecyclerActivity.this, CreateActivity.class);
+                     startActivity(intentCreate);
                     return true;
                 case R.id.navigation_find:
                     //mTextMessage.setText(R.string.title_find);
-                    Intent intentRecycler = new Intent(MainActivity.this, RecyclerActivity.class);
-                    startActivity(intentRecycler);
+                    //Intent intentRecycler = new Intent(CreateActivity.this, RecyclerActivity.class);
+                    //startActivity(intentRecycler);
                     return true;
                 case R.id.navigation_user:
-                  //  mTextMessage.setText(R.string.title_user);
-                    Intent intentUser = new Intent(MainActivity.this, UserActivity.class);
+                    //  mTextMessage.setText(R.string.title_user);
+                    Intent intentUser = new Intent(RecyclerActivity.this, UserActivity.class);
                     startActivity(intentUser);
                     return true;
             }
@@ -43,14 +43,15 @@ public class MainActivity extends AppCompatActivity {
     };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        TextView title = /*(TextView)*/ findViewById(R.id.message);
-        title.setText(R.string.nav_home);
-        //zmTextMessage = (TextView) findViewById(R.id.message);
+        setContentView(R.layout.recycler_activity);
+        TextView title = /*(TextView)*/ findViewById(R.id.recycler_act);
+        title.setText(R.string.nav_find);
+
         BottomNavigationView navigation = /*(BottomNavigationView)*/ findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
 
+    }
 }
+
--- UserActivity.java
+++ UserActivity.java
@@ -3,39 +3,38 @@ package com.example.cmfc_nav;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
-    //private TextView mTextMessage;
-
+public class UserActivity extends AppCompatActivity {
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     //mTextMessage.setText(R.string.title_home);
                     //dont need to open a new activity here
+                    Intent intentMain = new Intent(UserActivity.this, MainActivity.class);
+                    startActivity(intentMain);
                     return true;
                 case R.id.navigation_create:
                     //mTextMessage.setText(R.string.title_create);
-                    Intent intentCreate = new Intent(MainActivity.this, CreateActivity.class);
-                    startActivity(intentCreate);
+                     Intent intentCreate = new Intent(UserActivity.this, CreateActivity.class);
+                     startActivity(intentCreate);
                     return true;
                 case R.id.navigation_find:
                     //mTextMessage.setText(R.string.title_find);
-                    Intent intentRecycler = new Intent(MainActivity.this, RecyclerActivity.class);
+                    Intent intentRecycler = new Intent(UserActivity.this, RecyclerActivity.class);
                     startActivity(intentRecycler);
                     return true;
                 case R.id.navigation_user:
-                  //  mTextMessage.setText(R.string.title_user);
-                    Intent intentUser = new Intent(MainActivity.this, UserActivity.class);
-                    startActivity(intentUser);
+                    //  mTextMessage.setText(R.string.title_user);
+                   // Intent intentUser = new Intent(CreateActivity.this, UserActivity.class);
+                   // startActivity(intentUser);
                     return true;
             }
             return false;
@@ -43,14 +42,15 @@ public class MainActivity extends AppCompatActivity {
     };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        TextView title = /*(TextView)*/ findViewById(R.id.message);
-        title.setText(R.string.nav_home);
-        //zmTextMessage = (TextView) findViewById(R.id.message);
+        setContentView(R.layout.user_activity);
+        TextView title = /*(TextView)*/ findViewById(R.id.user_act);
+        title.setText(R.string.nav_user);
+
         BottomNavigationView navigation = /*(BottomNavigationView)*/ findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
 
+    }
 }
+
